482ee05c20e3a1cb66a061fa23e755c9cd20a594
Vitalik <soul33d@gmail.com>
2018-04-04 13:33:01 +0300

    added ng-bootstrap and bootstrap css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HotTableModule } from '@handsontable/angular'
+import { HotTableModule } from '@handsontable/angular';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
     HotTableModule
   ],
   providers: [],
