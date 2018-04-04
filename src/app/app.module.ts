59854ee7accca12f4056db4b605b133b279ae294
Vitalik <soul33d@gmail.com>
2018-04-04 12:57:36 +0300

    added handsontable module with wrapper, imported table styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HotTableModule } from '@handsontable/angular'
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HotTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
