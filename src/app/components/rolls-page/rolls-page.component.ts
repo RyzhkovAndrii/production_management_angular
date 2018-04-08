3ed79fe1e569f9ad8da375a5f4b8ee76f684b873
Vitalik <soul33d@gmail.com>
2018-04-08 19:56:16 +0300

    added styles for table, added table header

--- src/app/components/rolls-page/rolls-page.component.ts
+++ src/app/components/rolls-page/rolls-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-rolls-page',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rolls-page.component.css']
 })
 export class RollsPageComponent implements OnInit {
+  dateHeader: number[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    const daysInTable = 30;
+    const date = this.substructDays(new Date(), daysInTable);
+    for (let i = daysInTable; i > 0; i--) {
+      this.dateHeader.push(this.addDays(date, 1).getDate());
+    }
   }
 
+  private substructDays(date: Date, days: number): Date {
+    date.setDate(date.getDate() - days);
+    return date;
+  }
+
+  private addDays(date: Date, days: number): Date {
+    date.setDate(date.getDate() + days);
+    return date;
+  }
 }
