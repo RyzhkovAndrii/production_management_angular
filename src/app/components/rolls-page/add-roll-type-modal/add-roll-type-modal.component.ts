dcb6c1346f081572048f4b611985a52326331c3a
Vitalik <soul33d@gmail.com>
2018-04-22 14:58:33 +0300

    added preset colors for color picker

--- src/app/components/rolls-page/add-roll-type-modal/add-roll-type-modal.component.ts
+++ src/app/components/rolls-page/add-roll-type-modal/add-roll-type-modal.component.ts
@@ -1,6 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  NgbActiveModal
+} from '@ng-bootstrap/ng-bootstrap';
+import {
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-roll-type-modal',
@@ -10,17 +19,18 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class AddRollTypeModalComponent implements OnInit {
 
   form: FormGroup;
+  presetColors = ['#ffffff', '#000000', '#ff9d14', '#008a17', '#f1e972', '#edf100'];
 
   private readonly MIN_WEIGHT = 0.1;
   private readonly MIN_THICKNESS = 0.1;
 
-  constructor(public activeModal: NgbActiveModal) { }
+  constructor(public activeModal: NgbActiveModal) {}
 
   ngOnInit() {
     this.form = new FormGroup({
       name: new FormControl('', Validators.required),
       colorCode: new FormControl('#ffffff'),
-      thickness: new FormControl(undefined,[Validators.required, Validators.min(this.MIN_THICKNESS)]),
+      thickness: new FormControl(undefined, [Validators.required, Validators.min(this.MIN_THICKNESS)]),
       weight: new FormControl(undefined, [Validators.required, Validators.min(this.MIN_WEIGHT)])
     });
   }
