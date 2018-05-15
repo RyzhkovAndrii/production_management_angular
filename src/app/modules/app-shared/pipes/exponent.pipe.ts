28fccc6050c4e8d0b239575f21209fc0dbaf6749
Vitalik <soul33d@gmail.com>
2018-05-16 02:59:17 +0300

    added Decimal.js

--- src/app/modules/app-shared/pipes/exponent.pipe.ts
+++ src/app/modules/app-shared/pipes/exponent.pipe.ts
@@ -2,6 +2,9 @@ import {
   Pipe,
   PipeTransform
 } from '@angular/core';
+import {
+  Decimal
+} from 'decimal.js';
 
 @Pipe({
   name: 'exponent'
@@ -10,8 +13,8 @@ export class ExponentPipe implements PipeTransform {
 
   transform(value: number, degree: number = -3, digits: number = 3): string {
     if (typeof value === "number") {
-      const newLocal = value * Math.pow(10, degree);
-      return (newLocal).toFixed(digits);
+      const result = new Decimal(value).times(Math.pow(10, degree)); //value * Math.pow(10, degree);
+      return result.toFixed(digits);
     }
     return value;
   }
