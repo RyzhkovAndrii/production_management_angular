1f1d64c6a5650a238ba54f767dd29f0a3a119a67
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-05-24 16:38:03 +0300

    added order component

---
+++ src/app/modules/app-orders/components/order/order.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.css']
+})
+export class OrderComponent implements OnInit {
+
+  @Input() order: OrderDetails;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
