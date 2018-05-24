cd85fffa05d95e052065b4cd2b58c69333c1e712
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-05-24 16:40:28 +0300

    filled order component

--- src/app/modules/app-orders/components/orders-page/orders-page.component.ts
+++ src/app/modules/app-orders/components/orders-page/orders-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../../services/orders.service';
+import { Observable } from 'rxjs/Observable';
+import { ClientsService } from '../../services/client.service';
 
 @Component({
   selector: 'app-orders-page',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersPageComponent implements OnInit {
 
-  constructor() { }
+  orderDetailses: Observable<OrderDetails[]>;
+
+  constructor(private ordersService: OrdersService, private clientsService: ClientsService) { }
 
   ngOnInit() {
+    this.fetchData();    
   }
+
+  fetchData() {
+    this.orderDetailses = this.ordersService.getOrderDetailsList();
+  }  
 
 }
