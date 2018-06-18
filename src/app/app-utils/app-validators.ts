fc5ec1a7393d2fd05eae0b319dac89f52eb10ce6
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-06-18 22:30:56 +0300

    added common validateDecimalPlaces

--- src/app/app-utils/app-validators.ts
+++ src/app/app-utils/app-validators.ts
@@ -1,4 +1,7 @@
 import { FormControl } from "@angular/forms";
+import Decimal from "decimal.js";
+
+const DECIMAL_PLACES = 3; // todo common option
 
 export function integerValidator(control: FormControl) {
     if (control.value % 1 != 0) {
@@ -8,3 +11,12 @@ export function integerValidator(control: FormControl) {
     }
     return null;
 }
+
+export function validateDecimalPlaces(control: FormControl) {
+    if (control.value && !new Decimal(control.value).times(Math.pow(10, DECIMAL_PLACES)).isInteger()) {
+      return {
+        'decimalPlacesError': true
+      };
+    }
+    return null;
+  }
