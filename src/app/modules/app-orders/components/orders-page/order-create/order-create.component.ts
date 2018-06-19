ad7cedb02320820069ad2c28011c5d25859df2f5
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-06-19 19:14:48 +0300

    fixed adding options to product type select

--- src/app/modules/app-orders/components/orders-page/order-create/order-create.component.ts
+++ src/app/modules/app-orders/components/orders-page/order-create/order-create.component.ts
@@ -8,6 +8,7 @@ import { OrdersService } from '../../../services/orders.service';
 import { OrderItemService } from '../../../services/order-item.service';
 import Decimal from 'decimal.js';
 import { validateDecimalPlaces } from '../../../../../app-utils/app-validators';
+import { NgSelectComponent } from '@ng-select/ng-select';
 
 const MIN_PRODUCT_AMOUNT = 0.001;
 const DECIMAL_PLACES = 3; // todo common option
@@ -21,28 +22,23 @@ export class OrderCreateComponent implements OnInit {
 
   newItemDetailsList: { productType: ProductTypeResponse, amount: number }[] = [];
 
-  @Input()
-  productTypeList: ProductTypeResponse[];
+  @ViewChild('productTypeSelect') productTypeSelect: NgSelectComponent;
+
+  @Input() productTypeList: ProductTypeResponse[];
+  @Input() clientList: Client[];
+
   currentProductTypeList: ProductTypeResponse[];
 
-  @Input()
-  clientList: Client[];
-
-  @Output()
-  onSubmit = new EventEmitter<Order>();
-
-  @Output()
-  onCancel = new EventEmitter<any>();
-
-  @Output()
-  onClientListOpen = new EventEmitter<any>();
+  @Output() onSubmit = new EventEmitter<Order>();
+  @Output() onCancel = new EventEmitter<any>();
+  @Output() onClientListOpen = new EventEmitter<any>();
 
   form: FormGroup = new FormGroup({
     "client": new FormControl(null, [Validators.required]),
     "city": new FormControl(null, [Validators.required]),
     "date": new FormControl(null, [Validators.required]),
     "important": new FormControl(false, []),
-    "itemType": new FormControl(null, [Validators.required]),
+    "productType": new FormControl(null, [Validators.required]),
     "itemAmount": new FormControl(null, [Validators.required, Validators.min(MIN_PRODUCT_AMOUNT), validateDecimalPlaces])
   });
 
@@ -92,34 +88,44 @@ export class OrderCreateComponent implements OnInit {
       this.setAddOrderItemFormPristine();
     } else {
       this.showAddOrderItemErrors = false;
-      const { itemType, itemAmount } = this.form.value;
-      const productType: ProductTypeResponse = this.productTypeList.find(type => type.id === itemType);
+      const { productType, itemAmount } = this.form.value;
+      const productTypeDetails: ProductTypeResponse = this.productTypeList.find(type => type.id === productType);
       const integerItemAmount = new Decimal(itemAmount).times(Math.pow(10, DECIMAL_PLACES)).toNumber();
-      const itemDetails = { "productType": productType, "amount": integerItemAmount };
+      const itemDetails = { "productType": productTypeDetails, "amount": integerItemAmount };
       this.newItemDetailsList.push(itemDetails);
-      this.currentProductTypeList = this.currentProductTypeList.filter(type => type.id !== itemType);
+      this.removeOptionFromProductTypeSelect(itemDetails);
       this.resetAddOrderItemForm();
     }
   }
 
   removeNewItem(i: number) {
     const removedItem = this.newItemDetailsList.splice(i, 1)[0];
-    this.currentProductTypeList.push(removedItem.productType);
-    this.resetAddOrderItemForm(); // todo RESET select-list
+    this.addOptionToProductTypeSelect(removedItem);
+    this.resetAddOrderItemForm();
+  }
+
+  private addOptionToProductTypeSelect(item: { productType: ProductTypeResponse, amount: number }) {
+    this.currentProductTypeList.push(item.productType);
+    this.productTypeSelect.itemsList.setItems(this.currentProductTypeList);
+  }
+
+  private removeOptionFromProductTypeSelect(item: { productType: ProductTypeResponse, amount: number }) {
+    this.currentProductTypeList = this.currentProductTypeList.filter(type => type.id !== item.productType.id);
+    this.productTypeSelect.itemsList.setItems(this.currentProductTypeList);
   }
 
   private resetAddOrderItemForm() {
-    this.form.get("itemType").reset();
+    this.form.get("productType").reset();
     this.form.get("itemAmount").reset();
   }
 
   private setAddOrderItemFormPristine() {
-    this.form.get("itemType").markAsPristine();
+    this.form.get("productType").markAsPristine();
     this.form.get("itemAmount").markAsPristine();
   }
 
   private isAddOrderItemFormInvalid() {
-    return this.form.get("itemType").invalid || this.form.get("itemAmount").invalid;
+    return this.form.get("productType").invalid || this.form.get("itemAmount").invalid;
   }
 
   private showCreateMessage() {
