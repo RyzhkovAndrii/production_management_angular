50f0ddff0787e898800530434b03497384269de7
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-06-19 14:46:45 +0300

    added client delete confirm empty component

---
+++ src/app/modules/app-orders/components/orders-page/client-list/client-del-confirm/client-del-confirm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-del-confirm',
+  templateUrl: './client-del-confirm.component.html',
+  styleUrls: ['./client-del-confirm.component.css']
+})
+export class ClientDelConfirmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
