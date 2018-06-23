576c0d2005e043bd96ceac2b7878811e820826eb
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-06-24 00:17:41 +0300

    added get delivered order list methods

--- src/app/modules/app-orders/services/orders.service.ts
+++ src/app/modules/app-orders/services/orders.service.ts
@@ -10,6 +10,7 @@ import { httpErrorHandle } from '../../../app-utils/app-http-error-handler';
 import { Order } from '../models/order.model';
 import { OrderDetails } from '../models/order-details.model';
 import { OrderItem } from '../models/order-item.model';
+import { formatDateBrowserToServer, formatDate, formatDateServerToBrowser } from '../../../app-utils/app-date-utils';
 
 @Injectable()
 export class OrdersService {
@@ -21,8 +22,34 @@ export class OrdersService {
     private urlService: OrderModuleUrlService) { }
 
   getOrderList(): Observable<Order[]> {
-    const params = new HttpParams().set('_sort', 'deliveryDate'); // todo sort parameter in REST
-    return this.http.get(this.urlService.orderUrl, { params }).catch(httpErrorHandle);;
+    const params = new HttpParams()
+      .set('_sort', 'deliveryDate')
+      .set('isDelivered', 'false');
+    // params.append('_sort', 'deliveryDate'); // todo sort parameter in REST
+    // params.append('isDelivered', 'false');
+    return this.http.get(this.urlService.orderUrl, { params }).catch(httpErrorHandle);
+  }
+
+  getOrderListWithDelivered(startDate: Date): Observable<Order[]> {
+    console.log(formatDateServerToBrowser(formatDate(startDate)));
+    const obsFirst$ = this.getOrderList();
+    const params = new HttpParams()
+      .set('_sort', 'deliveryDate') // todo sort parameter in REST
+      .set('isDelivered', 'true') // todo in REST
+      // .set('showDeliveredStartDate', formatDateBrowserToServer(formatDate(startDate))); // todo in REST
+      .set('actualDeliveryDate_gte', formatDateServerToBrowser(formatDate(startDate)));
+    const obsSecond$ = this.http.get(this.urlService.orderUrl, { params }).catch(httpErrorHandle);
+    return Observable
+      .forkJoin(obsFirst$, obsSecond$)
+      .map(([data1, data2]) => [...data1, ...data2])
+      .map(data => data.sort(this._compareFn))
+      .catch(httpErrorHandle);
+  }
+
+  private _compareFn(first: Order, second: Order): number {
+    if (first.deliveryDate > second.deliveryDate) return 1;
+    if (first.deliveryDate < second.deliveryDate) return -1;
+    return 0;
   }
 
   save(order: Order): Observable<Order> {
