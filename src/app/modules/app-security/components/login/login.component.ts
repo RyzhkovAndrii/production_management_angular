c2228f9fba222ba71668d6e721fa1eb0c8fe62eb
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-07-22 21:23:10 +0300

    added login and allert for exception

--- src/app/modules/app-security/components/login/login.component.ts
+++ src/app/modules/app-security/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthenticationService } from '../../services/authentication.service';
-import { Router } from '../../../../../../node_modules/@angular/router';
+import { HttpErrorResponse } from '../../../../../../node_modules/@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +12,8 @@ export class LoginComponent implements OnInit {
 
   @Output() login = new EventEmitter<any>();
 
+  message?: String;
+
   constructor(
     private authService: AuthenticationService
   ) { }
@@ -21,14 +23,51 @@ export class LoginComponent implements OnInit {
 
   form: FormGroup = new FormGroup({
     'username': new FormControl(null, [Validators.required]),
-    'password': new FormControl(null, [Validators.required])
+    'password': new FormControl(null, [Validators.required]),
+    'remember': new FormControl(false)
   });
 
   submit() {
-    const {username, password} = this.form.value;
-    this.authService.login(username, password);
-    console.log(this.authService.getCurrentUser());
-    this.login.emit();
+    const { username, password } = this.form.value;
+    this.authService
+      .login(username, password)
+      .subscribe(
+        token => {
+          this.authService.setToken(token);
+          this.authService.recieveCurrentUserInfo()
+            .subscribe(
+              user => {
+                this.authService.setCurrentUser(user);
+                this.hideAllert();
+                this.login.emit();
+              }
+            )
+        },
+        (err: HttpErrorResponse) => {
+          this.message = this.convertErrorMessage(JSON.parse(err.error).message);
+        }
+      );
+  }
+
+  private convertErrorMessage(message: string): string {
+    switch(message) {
+      case 'User is not specified': {
+        return 'Имя пользователя не указано'
+      }
+      case 'User does not exist': {
+        return 'Имя пользователя не найдено'
+      }
+      case 'Password is incorrect': {
+        return 'Пароль неверен'
+      }
+      default: {
+        return message;
+      }
+    }
+  }
+
+  hideAllert() {
+    this.message = null;
   }
 
 }
