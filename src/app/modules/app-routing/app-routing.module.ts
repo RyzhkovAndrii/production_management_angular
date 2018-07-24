0c9907126430ff70e0fc4f2240860ff7af805361
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-07-24 22:53:18 +0300

    added role guard and roles to routes

--- src/app/modules/app-routing/app-routing.module.ts
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -9,39 +9,64 @@ import {
 import {
   HomePageComponent
 } from '../../components/home-page/home-page.component';
+import {
+  RoleGuard
+} from '../app-security/guards/role.guard';
 
 const appRoutes: Routes = [{
-    path: '',
-    component: HomePageComponent
-  },
-  {
-    path: 'auth',
-    loadChildren: '../app-security/security.module#SecurityModule'
-  },
-  {
-    path: 'rolls',
-    loadChildren: '../app-rolls/rolls.module#RollsModule'
-  },
-  {
-    path: 'products',
-    loadChildren: '../app-products/products.module#ProductsModule'
-  },
-  {
-    path: 'orders',
-    loadChildren: '../app-orders/orders.module#OrdersModule'
-  },
-  {
-    path: 'standards',
-    loadChildren: '../app-standards/standards.module#StandardsModule'
-  },
-  {
-    path: 'products-plan',
-    loadChildren: '../app-products-plan/products-plan.module#ProductsPlanModule'
-  },
-  {
-    path: '**', // todo check without spring redirect
-    redirectTo: ''
+  path: '',
+  component: HomePageComponent
+},
+{
+  path: 'auth',
+  loadChildren: '../app-security/security.module#SecurityModule'
+},
+{
+  path: 'rolls',
+  loadChildren: '../app-rolls/rolls.module#RollsModule',
+  canActivate: [RoleGuard],
+  data: {
+    roles: ['ROLE_TECHNOLOGIST', 'ROLE_CMO', 'ROLE_CTO',
+      'ROLE_ACOUNTER', 'ROLE_ECONOMIST', 'ROLE_STOREKEEPER']
   }
+},
+{
+  path: 'products',
+  loadChildren: '../app-products/products.module#ProductsModule',
+  canActivate: [RoleGuard],
+  data: {
+    roles: ['ROLE_TECHNOLOGIST', 'ROLE_MANAGER', 'ROLE_CMO', 'ROLE_CTO',
+      'ROLE_ACOUNTER', 'ROLE_ECONOMIST', 'ROLE_STOREKEEPER']
+  }
+},
+{
+  path: 'orders',
+  loadChildren: '../app-orders/orders.module#OrdersModule',
+  canActivate: [RoleGuard],
+  data: {
+    roles: ['ROLE_MANAGER', 'ROLE_CMO', 'ROLE_ECONOMIST']
+  }
+},
+{
+  path: 'standards',
+  loadChildren: '../app-standards/standards.module#StandardsModule',
+  canActivate: [RoleGuard],
+  data: {
+    roles: ['ROLE_TECHNOLOGIST', 'ROLE_CMO', 'ROLE_CTO', 'ROLE_ECONOMIST']
+  }
+},
+{
+  path: 'products-plan',
+  loadChildren: '../app-products-plan/products-plan.module#ProductsPlanModule',
+  canActivate: [RoleGuard],
+  data: {
+    roles: ['ROLE_TECHNOLOGIST', 'ROLE_CMO', 'ROLE_CTO', 'ROLE_ECONOMIST', 'ROLE_MANAGER']
+  }
+},
+{
+  path: '**', // todo check without spring redirect
+  redirectTo: ''
+}
 ];
 
 @NgModule({
@@ -51,4 +76,4 @@ const appRoutes: Routes = [{
   exports: [RouterModule],
   declarations: []
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
