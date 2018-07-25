a7e000b952324a2e52ad2196c2b0c10a0c99a83d
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-07-26 00:03:24 +0300

    added modification model

---
+++ src/app/modules/app-users/models/modification.model.ts
@@ -0,0 +1,11 @@
+import { TableType } from "./table-type.enum";
+
+export class Modififcation {
+
+    constructor(
+        public userId: number,
+        public modificationDateTime: string,
+        public tableType: TableType,
+    ) { }
+
+}
