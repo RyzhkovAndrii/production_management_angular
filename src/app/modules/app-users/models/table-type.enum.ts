0cadc3fe5456cfee0511ea1462e4ed8adc1b5d13
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-07-25 23:58:26 +0300

    added enum of table types

---
+++ src/app/modules/app-users/models/table-type.enum.ts
@@ -0,0 +1,10 @@
+export enum TableType {
+
+    ROLLS = 'ROLLS',
+    PRODUCTS = 'PRODUCTS',
+    ORDERS = 'ORDERS',
+    NORMS = 'NORMS',
+    ROLL_PLANS = 'ROLL_PLANS',
+    PRODUCT_PLANS = 'PRODUCT_PLANS'
+
+}
