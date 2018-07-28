cb786475aafb3e26103305ecb35d87c7cb6c581c
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-07-28 14:16:13 +0300

    filled last modification component

--- src/app/modules/app-users/components/last-modification/last-modification.component.ts
+++ src/app/modules/app-users/components/last-modification/last-modification.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewContainerRef } from '@angular/core';
+import { ModalDialogService } from '../../../../../../node_modules/ngx-modal-dialog';
+
+import { User } from '../../models/user.model';
+import { Modififcation } from '../../models/modification.model';
+import { ModificationService } from '../../services/modification.service';
+import { TableType } from '../../models/table-type.enum';
+import { UserService } from '../../services/user.service';
+import { AppModalService } from '../../../app-shared/services/app-modal.service';
 
 @Component({
   selector: 'app-last-modification',
@@ -7,9 +15,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LastModificationComponent implements OnInit {
 
-  constructor() { }
+  @Input() tableType: TableType;
+
+  lastModification: Modififcation;
+  lastUser: User;
+
+  constructor(
+    private modificationService: ModificationService,
+    private userService: UserService,
+    private viewRef: ViewContainerRef,
+    private ngxModalDialogService: ModalDialogService,
+    private appModalService: AppModalService
+  ) { }
 
   ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.modificationService
+      .get(this.tableType)
+      .subscribe(
+        response => {
+          this.lastModification = response;
+          this.userService
+            .get(response.userId)
+            .subscribe(
+              user => this.lastUser = user,
+              error => this.appModalService.openHttpErrorModal(this.ngxModalDialogService, this.viewRef, error)
+            );
+        },
+        error => this.appModalService.openHttpErrorModal(this.ngxModalDialogService, this.viewRef, error)
+      )
+  }
+
+  reload() {
+    this.fetchData();
   }
 
 }
