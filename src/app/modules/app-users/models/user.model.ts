fd20e36dda3bfab70830149380bf02ccf117665f
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-07-30 08:58:20 +0300

    removed password field form user model

--- src/app/modules/app-users/models/user.model.ts
+++ src/app/modules/app-users/models/user.model.ts
@@ -4,7 +4,6 @@ export class User {
 
     constructor(
         public username: string,
-        public password: string,
         public firstName?: string,
         public lastName?: string,
         public roles?: Role[],
