1f530ca170922f083fdc08447434a5673bf84d91
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-08-01 14:46:49 +0300

    added productTypeId field

--- src/app/modules/app-machines/models/machine-plan-item.model.ts
+++ src/app/modules/app-machines/models/machine-plan-item.model.ts
@@ -1,11 +1,12 @@
 export class MachinePlanItem {
 
-    constructor(
-        public machineNumber: number,
-        public timeStart: string,
-        public productType: number | ProductTypeResponse,
-        public productAmount: number,
-        public duration?: number
-    ) { }
+    public machineNumber: number;
+    public timeStart: string;
+    public productTypeId: number;
+    public productAmount: number;
+    public duration?: number;
+    public productType?: ProductTypeResponse;
+
+    constructor() { }
 
 }
