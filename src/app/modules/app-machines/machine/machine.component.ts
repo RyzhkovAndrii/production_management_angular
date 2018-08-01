d89afa4baba1a9baf2538b8102e7d5b199c66258
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-08-01 14:48:25 +0300

    added initial data fetching

--- src/app/modules/app-machines/machine/machine.component.ts
+++ src/app/modules/app-machines/machine/machine.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MachineService } from '../services/machine.service';
+import { MachinePlanItem } from '../models/machine-plan-item.model';
+import { ProductsService } from '../../app-products/services/products.service';
 
 @Component({
   selector: 'app-machine',
@@ -7,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MachineComponent implements OnInit {
 
-  constructor() {
-  }
+  @Input() public machineNumber: number;
+
+  public machinePlan: MachinePlanItem[];
+
+  constructor(
+    private machineService: MachineService,
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit() {
+    this.fetchPlanData();
   }
+
+  private fetchPlanData() {
+    this.machineService
+      .getAll(this.machineNumber)
+      .subscribe(response => {
+        this.machinePlan = response;
+        this.machinePlan.map(planItem => this.getDetails(planItem))
+      });
+  }
+
+  private getDetails(planItem: MachinePlanItem): MachinePlanItem {
+    this.productsService
+      .getProductType(planItem.productTypeId)
+      .subscribe(response => planItem.productType = response);
+    return planItem;
+  }
+
+
 
 }
