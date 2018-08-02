acd861f6d0eb2f7cafd62bbd6090a53ee6b14607
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-08-02 14:30:54 +0300

    filled machine component

--- src/app/modules/app-machines/machine/machine.component.ts
+++ src/app/modules/app-machines/machine/machine.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import * as moment from 'moment';
+
 import { MachineService } from '../services/machine.service';
 import { MachinePlanItem } from '../models/machine-plan-item.model';
-import { ProductsService } from '../../app-products/services/products.service';
 
 @Component({
   selector: 'app-machine',
@@ -12,11 +13,11 @@ export class MachineComponent implements OnInit {
 
   @Input() public machineNumber: number;
 
+  public filledMachinePlan: MachinePlanItem[] = [];
   public machinePlan: MachinePlanItem[];
 
   constructor(
-    private machineService: MachineService,
-    private productsService: ProductsService
+    private machineService: MachineService
   ) { }
 
   ngOnInit() {
@@ -28,17 +29,56 @@ export class MachineComponent implements OnInit {
       .getAll(this.machineNumber)
       .subscribe(response => {
         this.machinePlan = response;
-        this.machinePlan.map(planItem => this.getDetails(planItem))
+        this.fillPlan();
       });
   }
 
-  private getDetails(planItem: MachinePlanItem): MachinePlanItem {
-    this.productsService
-      .getProductType(planItem.productTypeId)
-      .subscribe(response => planItem.productType = response);
-    return planItem;
+  private fillPlan() {
+    var diff;
+    var nextStart;
+    const firstPlanTime = this.getDateTimeStart(this.machinePlan[0]);
+    const firstStart = moment(firstPlanTime);
+    const todayMidnight = moment(firstPlanTime).startOf('day');
+    const tomorrowMidnight = moment(firstPlanTime).endOf('day');
+    diff = this.getDiffInHours(todayMidnight, firstStart);
+    if (diff > 0) {
+      const empty = this.getEmptyPlan(diff);
+      this.filledMachinePlan.push(empty);
+    }
+    for (var i = 0; i < this.machinePlan.length; i++) {
+      this.filledMachinePlan.push(this.machinePlan[i]);
+      const currentEnd = moment(this.getDateTimeFinish(this.machinePlan[i]));
+      if (i === this.machinePlan.length - 1) {
+        nextStart = tomorrowMidnight;
+      } else {
+        nextStart = moment(this.getDateTimeStart(this.machinePlan[i + 1]));
+      }
+      diff = this.getDiffInHours(currentEnd, nextStart);
+      if (diff > 0) {
+        const empty = this.getEmptyPlan(diff);
+        this.filledMachinePlan.push(empty);
+      }
+    }
   }
 
+  private getEmptyPlan(duration: number) {
+    let empty = new MachinePlanItem();
+    empty.duration = duration;
+    return empty;
+  }
 
+  private getDateTimeStart(planItem: MachinePlanItem): Date {
+    const format = 'DD-MM-YYYY HH:mm';
+    return moment(planItem.timeStart, format).toDate();
+  }
+
+  private getDateTimeFinish(planItem: MachinePlanItem): Date {
+    const dateTimeStart = this.getDateTimeStart(planItem);
+    return moment(dateTimeStart).add(planItem.duration, 'hour').toDate();
+  }
+
+  private getDiffInHours(before, after): number {
+    return moment.duration(after.diff(before)).asHours();;
+  }
 
 }
