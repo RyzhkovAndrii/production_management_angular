a4a27b5f18d55733314d01c113825c4b743922e4
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-08-02 14:31:58 +0300

    added route on machine page component

--- src/app/modules/app-machines/machines-routing.module.ts
+++ src/app/modules/app-machines/machines-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { MachinesPageComponent } from "./machines-page/machines-page.component";
 
 const machinesRoutes: Routes = [{
-    
+    path: '',
+    component: MachinesPageComponent
 }];
 
 @NgModule({
