8c6279f0e54cb1241f33e5d90e45dba1693350ed
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-08-06 17:29:31 +0300

    added date select form

--- src/app/modules/app-machines/machines-page/machines-page.component.ts
+++ src/app/modules/app-machines/machines-page/machines-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-machines-page',
@@ -8,12 +10,22 @@ import { Component, OnInit } from '@angular/core';
 export class MachinesPageComponent implements OnInit {
 
   hours: number[];
+  selectedDate: Date = new Date();
+
+  dateForm = new FormGroup({
+    'date': new FormControl(moment().format('YYYY-MM-DD'), [Validators.required])
+  })
 
   constructor() {
     this.hours = Array(24).fill(0).map((x, i) => i);
   }
 
   ngOnInit() {
+  }
+
+  dateChange() {
+    const { date } = this.dateForm.value;
+    this.selectedDate = date;
   }
 
 }
