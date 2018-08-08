df594839d6a302eab581c494039d8b356b416b16
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-08-08 16:26:39 +0300

    filled daily plan table component

--- src/app/modules/app-machines/machines-page/daily-plan-table/daily-plan-table.component.ts
+++ src/app/modules/app-machines/machines-page/daily-plan-table/daily-plan-table.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewContainerRef } from '@angular/core';
+import { DailyProductPlanService } from '../../services/daily-product-plan.service';
+import { Observable } from '../../../../../../node_modules/rxjs';
+import { ModalDialogService } from '../../../../../../node_modules/ngx-modal-dialog';
+import { AppModalService } from '../../../app-shared/services/app-modal.service';
 
 @Component({
   selector: 'app-daily-plan-table',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DailyPlanTableComponent implements OnInit {
 
-  constructor() { }
+  @Input() dailyPlans: ProductPlanBatchResponse[];
+  productTypes: ProductTypeResponse[] = [];
+
+  isFetched = false;
+
+  constructor(
+    private planService: DailyProductPlanService,
+    private viewRef: ViewContainerRef,
+    private ngxModalDialogService: ModalDialogService,
+    private appModalService: AppModalService
+  ) { }
 
   ngOnInit() {
+    this.fetchProductTypes();
+  }
+
+  fetchProductTypes() {
+    this.isFetched = false;
+    const obs: Observable<ProductTypeResponse>[] = [];
+    this.dailyPlans.forEach(plan => obs.push(this.planService.getProductType(plan.productTypeId)));
+    Observable
+      .forkJoin(obs)
+      .subscribe(
+        response => {
+          response.forEach(type => this.productTypes.push(type));
+        },
+        error => this.appModalService.openHttpErrorModal(this.ngxModalDialogService, this.viewRef, error))
+  }
+
+  amountDiff(plan: ProductPlanBatchResponse) {
+    return plan.manufacturedAmount - plan.productToMachinePlane;
   }
 
 }
