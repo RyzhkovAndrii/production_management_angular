12f162f801e7c60b3c9de1532e3f3bfd3473a3cd
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-08-10 18:36:50 +0300

    added get rollType with cash

--- src/app/modules/app-machines/services/daily-product-plan.service.ts
+++ src/app/modules/app-machines/services/daily-product-plan.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from "@angular/core";
 import { ProductsService } from "../../app-products/services/products.service";
 import { Observable } from "../../../../../node_modules/rxjs";
+import { RollsService } from "../../app-rolls/services/rolls.service";
 
 @Injectable()
 export class DailyProductPlanService {
 
     private productTypeCash: ProductTypeResponse[] = [];
+    private rollTypeCash: RollType[] = [];
 
     constructor(
-        private productService: ProductsService
+        private productService: ProductsService,
+        private rollService: RollsService
     ) { }
 
     getProductType(id: number): Observable<ProductTypeResponse> {
@@ -18,6 +21,15 @@ export class DailyProductPlanService {
             : this.productService
                 .getProductType(id)
                 .do(response => this.productTypeCash.push(response));
+    }
+
+    getRollType(id: number): Observable<RollType> {
+        const cash = this.rollTypeCash.find(rollType => rollType.id == id);
+        return cash
+            ? Observable.of(cash)
+            : this.rollService
+                .getRollType(id)
+                .do(response => this.rollTypeCash.push(response));
     }
 
 }
