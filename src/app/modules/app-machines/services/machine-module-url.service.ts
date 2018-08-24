8d7b33a48d89544af16b0ecc99ddba070ad0f0fa
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-08-24 11:31:07 +0300

    formatted code

--- src/app/modules/app-machines/services/machine-module-url.service.ts
+++ src/app/modules/app-machines/services/machine-module-url.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@angular/core";
-import { RestDetailsService } from "../../../services/rest-details-service";
+import { Injectable } from '@angular/core';
+import { RestDetailsService } from '../../../services/rest-details-service';
 
 @Injectable()
 export class MachineModuleUrlService {
@@ -7,8 +7,8 @@ export class MachineModuleUrlService {
     private host = this.restDetails.host;
 
     machinePlanUrl = `${this.host}/machine-plans`;
-    machinePlanItemFullUrl = `${this.host}/machine-plan-items`; 
-    machinePlanItemUrl = 'machine-plan-items'; 
+    machinePlanItemFullUrl = `${this.host}/machine-plan-items`;
+    machinePlanItemUrl = 'machine-plan-items';
 
     constructor(private restDetails: RestDetailsService) { }
 
