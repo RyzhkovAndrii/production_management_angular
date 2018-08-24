2c37f8630b965a71b844085ca9547be3f8a85f0f
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-08-24 11:30:44 +0300

    added save plan with planItems method, added auto open error window

--- src/app/modules/app-machines/services/machine-plan-item.service.ts
+++ src/app/modules/app-machines/services/machine-plan-item.service.ts
@@ -1,25 +1,33 @@
-import { Injectable } from "@angular/core";
-import { Observable } from "../../../../../node_modules/rxjs";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Observable } from '../../../../../node_modules/rxjs';
+import { HttpClient } from '@angular/common/http';
 
-import { MachineModuleUrlService } from "./machine-module-url.service";
-import { httpErrorHandle } from "../../../app-utils/app-http-error-handler";
-import { MachinePlanItem } from "../models/machine-plan-item.model";
-import appHeaders from "../../../app-utils/app-headers";
+import appHeaders from '../../../app-utils/app-headers';
+import { MachineModuleUrlService } from './machine-module-url.service';
+import { MachinePlanItem } from '../models/machine-plan-item.model';
+import { AppHttpErrorService } from '../../app-shared/services/app-http-error.service';
 
 @Injectable()
 export class MachinePlanItemService {
 
     constructor(
         private http: HttpClient,
-        private urlService: MachineModuleUrlService
+        private urlService: MachineModuleUrlService,
+        private httpErrorService: AppHttpErrorService
     ) { }
 
     getAll(machinePlanId: number): Observable<MachinePlanItem[]> {
         const url = `${this.urlService.machinePlanUrl}/${machinePlanId}/${this.urlService.machinePlanItemUrl}`;
         return this.http
             .get(url, { headers: appHeaders })
-            .catch(httpErrorHandle);
+            .catch(err => this.httpErrorService.openHttpErrorWindow(err));
+    }
+
+    save(machinePlanId: number, planItem: MachinePlanItem): Observable<MachinePlanItem> {
+        const url = `${this.urlService.machinePlanUrl}/${machinePlanId}/${this.urlService.machinePlanItemUrl}`;
+        return this.http
+            .post(url, planItem, { headers: appHeaders })
+            .catch(err => this.httpErrorService.openHttpErrorWindow(err));
     }
 
 }
--- src/app/modules/app-machines/services/machine-plan.service.ts
+++ src/app/modules/app-machines/services/machine-plan.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from "@angular/core";
-import { Observable } from "../../../../../node_modules/rxjs";
-import { MachinePlan } from "../models/machine-plan.model";
-import { HttpParams, HttpClient } from "@angular/common/http";
-import { MachineModuleUrlService } from "./machine-module-url.service";
-import appHeaders from "../../../app-utils/app-headers";
-import { httpErrorHandle } from "../../../app-utils/app-http-error-handler";
-import { formatDate } from "../../../app-utils/app-date-utils";
-import { MachinePlanItemService } from "./machine-plan-item.service";
+import { Injectable } from '@angular/core';
+import { Observable } from '../../../../../node_modules/rxjs';
+import { HttpParams, HttpClient } from '@angular/common/http';
+
+import appHeaders from '../../../app-utils/app-headers';
+import { formatDate } from '../../../app-utils/app-date-utils';
+import { MachinePlan } from '../models/machine-plan.model';
+import { MachineModuleUrlService } from './machine-module-url.service';
+import { MachinePlanItemService } from './machine-plan-item.service';
+import { AppHttpErrorService } from '../../app-shared/services/app-http-error.service';
 
 @Injectable()
 export class MachinePlanService {
@@ -14,17 +15,18 @@ export class MachinePlanService {
     constructor(
         private http: HttpClient,
         private planItemService: MachinePlanItemService,
-        private urlService: MachineModuleUrlService
+        private urlService: MachineModuleUrlService,
+        private httpErrorService: AppHttpErrorService
     ) { }
 
     getAll(date: Date, machineNumber: number): Observable<MachinePlan[]> {
-        let params = new HttpParams()
+        const params = new HttpParams()
             .set('sort', 'timeStart')
             .set('machine_number', machineNumber.toString())
             .set('date', formatDate(date));
         return this.http
             .get(this.urlService.machinePlanUrl, { params, headers: appHeaders })
-            .catch(httpErrorHandle);
+            .catch(err => this.httpErrorService.openHttpErrorWindow(err));
     }
 
     getAllWithItems(date: Date, machineNumber: number): Observable<MachinePlan[]> {
@@ -39,21 +41,35 @@ export class MachinePlanService {
                                 .map(items => {
                                     plan.planItems = items;
                                     return plan;
-                                })
+                                });
                         })
-                    )
-            })
+                    );
+            });
     }
 
     save(plan: MachinePlan): Observable<MachinePlan> {
         return this.http
             .post(this.urlService.machinePlanUrl, plan, { headers: appHeaders })
-            .catch(httpErrorHandle);
+            .catch(err => this.httpErrorService.openHttpErrorWindow(err));
+    }
+
+    saveWithItems(plan: MachinePlan): Observable<MachinePlan> {
+        return this.save(plan)
+            .flatMap(planResp =>
+                Observable
+                    .forkJoin(plan.planItems.map(item => this.planItemService.save(planResp.id, item)))
+                    .map(respItems => {
+                        planResp.planItems = respItems;
+                        return planResp;
+                    })
+            );
     }
 
     delete(id: number) {
         const url = `${this.urlService.machinePlanUrl}/${id}`;
-        return this.http.delete(url, { headers: appHeaders }).catch(httpErrorHandle);
-      }
+        return this.http
+            .delete(url, { headers: appHeaders })
+            .catch(err => this.httpErrorService.openHttpErrorWindow(err));
+    }
 
 }
