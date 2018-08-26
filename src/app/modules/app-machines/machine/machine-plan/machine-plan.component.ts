3367f6f022072d36e3a78ff11f5001f5e1df73d4
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-08-26 22:12:21 +0300

    refactored machine plan component

--- src/app/modules/app-machines/machine/machine-plan/machine-plan.component.ts
+++ src/app/modules/app-machines/machine/machine-plan/machine-plan.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { MachinePlan } from '../../models/machine-plan.model';
-import { Observable } from 'rxjs/Observable';
-import { MachineModuleCasheService } from '../../services/machine-module-cashe.service';
 
 @Component({
   selector: 'app-machine-plan',
@@ -12,36 +10,17 @@ import { MachineModuleCasheService } from '../../services/machine-module-cashe.s
 export class MachinePlanComponent implements OnInit {
 
   @Input() plan: MachinePlan;
-  plan$: Observable<MachinePlan>;
 
   color: string;
   width: number;
   isPlanEmpty: boolean;
 
-  constructor(
-    private casheService: MachineModuleCasheService
-  ) { }
+  constructor() { }
 
   ngOnInit() {
     this.isPlanEmpty = this.plan.productTypeId === undefined;
     this.width = this.plan.duration * 90 / 24;
     this.color = this.isPlanEmpty ? 'grey' : this.plan.isImportant ? '#de3163' : 'white';
-    this.plan$ = this.getDetailedPlan(this.plan);
-  }
-
-  private getDetailedPlan(plan: MachinePlan): Observable<MachinePlan> {
-    return this.isPlanEmpty
-      ? Observable.of(plan)
-      : Observable.of(this.plan)
-        .flatMap(obsPlan => {
-          return this.casheService
-            .getProductType(obsPlan.productTypeId)
-            .map(productType => {
-              console.log(productType);
-              plan.productType = productType;
-              return plan;
-            });
-        });
   }
 
 }
