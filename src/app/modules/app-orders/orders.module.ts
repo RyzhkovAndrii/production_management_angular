52cc87e100fd445e4ae0c29eda04bcf0d23e4407
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2018-09-12 14:27:48 +0300

    added can acces to orders module (added rolls to actions)

--- src/app/modules/app-orders/orders.module.ts
+++ src/app/modules/app-orders/orders.module.ts
@@ -1,27 +1,28 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { NgSelectModule } from "@ng-select/ng-select";
-import { ContextMenuModule } from "ngx-contextmenu";
-import { ModalDialogModule } from "ngx-modal-dialog";
-import { AppSharedModule } from "../app-shared/app-shared.module";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { ContextMenuModule } from 'ngx-contextmenu';
+import { ModalDialogModule } from 'ngx-modal-dialog';
+import { AppSharedModule } from '../app-shared/app-shared.module';
 import { NgDatepickerModule } from 'ng2-datepicker';
 
-import { OrderModuleUrlService } from "./services/order-module-url.service";
-import { OrdersService } from "./services/orders.service";
-import { ProductsUrlsService } from "../app-products/services/products-urls.service";
-import { ClientsService } from "./services/client.service";
-import { ProductsService } from "../app-products/services/products.service";
-import { OrderItemService } from "./services/order-item.service";
-import { OrdersPageComponent } from "./components/orders-page/orders-page.component";
-import { OrdersRoutingModule } from "./orders-routing.module";
+import { OrderModuleUrlService } from './services/order-module-url.service';
+import { OrdersService } from './services/orders.service';
+import { ProductsUrlsService } from '../app-products/services/products-urls.service';
+import { ClientsService } from './services/client.service';
+import { ProductsService } from '../app-products/services/products.service';
+import { OrderItemService } from './services/order-item.service';
+import { OrdersPageComponent } from './components/orders-page/orders-page.component';
+import { OrdersRoutingModule } from './orders-routing.module';
 import { OrderCreateComponent } from './components/orders-page/order-create/order-create.component';
 import { ClientListComponent } from './components/orders-page/client-list/client-list.component';
 import { OrderEditComponent } from './components/orders-page/order-edit/order-edit.component';
-import { OrderComponent } from "./components/orders-page/order/order.component";
-import { OrderLeftoverProductComponent } from "./components/orders-page/order-leftover-product/order-leftover-product.component";
+import { OrderComponent } from './components/orders-page/order/order.component';
+import { OrderLeftoverProductComponent } from './components/orders-page/order-leftover-product/order-leftover-product.component';
 import { OrderDeliveredShowComponent } from './components/orders-page/order-delivered-show/order-delivered-show.component';
-import { OrderDeliveryConfirmComponent } from "./components/orders-page/order/order-delivery-confirm/order-delivery-confirm.component";
+import { OrderDeliveryConfirmComponent } from './components/orders-page/order/order-delivery-confirm/order-delivery-confirm.component';
+import { SecurityModule } from '../app-security/security.module';
 
 @NgModule({
     imports: [
@@ -35,7 +36,8 @@ import { OrderDeliveryConfirmComponent } from "./components/orders-page/order/or
         }),
         ModalDialogModule.forRoot(),
         AppSharedModule,
-        NgDatepickerModule
+        NgDatepickerModule,
+        SecurityModule
     ],
     declarations: [
         OrdersPageComponent,
