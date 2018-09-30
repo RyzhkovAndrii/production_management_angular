6872671a94091fb2659104ca623fee0c7999ca5a
Vitalik <soul33d@gmail.com>
2018-09-30 19:43:43 +0300

    validateDecimalPlaces moved to newDecimalPlacesValidator() builder func

--- src/app/app-utils/app-validators.ts
+++ src/app/app-utils/app-validators.ts
@@ -1,20 +1,34 @@
-import { FormControl } from "@angular/forms";
+import {
+  FormControl
+} from "@angular/forms";
 import * as moment from "moment";
+import Decimal from "decimal.js";
 
 export function integerValidator(control: FormControl) {
-    if (control.value % 1 != 0) {
-        return {
-            'notIntegerError': true
-        };
-    }
-    return null;
+  if (control.value % 1 != 0) {
+    return {
+      'notIntegerError': true
+    };
+  }
+  return null;
 }
 
 export function validateDateNotAfterCurrent(control: FormControl) {
-    if(control.value && moment(control.value, 'YYYY-MM-DD').isAfter(moment())) {
-        return {
-            'afterCurrentDateError': true
-        };
+  if (control.value && moment(control.value, 'YYYY-MM-DD').isAfter(moment())) {
+    return {
+      'afterCurrentDateError': true
+    };
+  }
+  return null;
+}
+export function newDecimalPlacesValidator(decimalPlaces: number): (control: FormControl) => any {
+  const validator = (control: FormControl) => {
+    if (control.value && !new Decimal(control.value).times(Math.pow(10, decimalPlaces)).isInteger()) {
+      return {
+        'decimalPlacesError': true
+      };
     }
     return null;
   }
+  return validator;
+}
