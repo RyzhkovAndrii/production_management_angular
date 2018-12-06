629ba83c415e21280f87d9ee237f9b6283e5d38b
Vitalik <soul33d@gmail.com>
2018-12-07 01:35:47 +0200

    added for table rows model mapping service method RollPlanInfo[] get

--- src/app/modules/app-rolls-plan/services/rolls-plan.service.ts
+++ src/app/modules/app-rolls-plan/services/rolls-plan.service.ts
@@ -19,6 +19,14 @@ import appHeaders from '../../../app-utils/app-headers';
 import {
   httpErrorHandle
 } from '../../../app-utils/app-http-error-handler';
+import {
+  formatDate,
+  addDays,
+  midnightDate
+} from '../../../app-utils/app-date-utils';
+import {
+  from
+} from 'rxjs/observable/from';
 
 @Injectable()
 export class RollsPlanService {
@@ -26,6 +34,46 @@ export class RollsPlanService {
   constructor(private urls: RollsPlanUrlsService,
     private http: HttpClient,
     private rollsService: RollsService) {}
+
+  getRollPlansInfo(fromDate: Date, toDate: Date): Observable < RollPlanInfo[] > {
+    const weeklyDate = formatDate(addDays(fromDate, 7));
+    const beginDate = formatDate(fromDate);
+    const endDate = formatDate(toDate);
+    const currentDate = formatDate(midnightDate());
+
+    return this.rollsService.getRollTypes()
+      .flatMap(rolls => this.getBatchesByRange(beginDate, endDate)
+        .flatMap(batches => this.getRollPlanLeftoversWithoutPlan(currentDate).map(this.convertOversToMap)
+          .flatMap(currentOversMap => this.getRollPlanLeftoversWithoutPlan(weeklyDate).map(this.convertOversToMap)
+            .flatMap(weeklyOversMap => this.getRollPlanLeftoversTotal(weeklyDate).map(this.convertOversToMap)
+              .flatMap(weeklyTotalOversMap => this.getRollPlanLeftoversWithoutPlan(endDate).map(this.convertOversToMap)
+                .flatMap(toOversMap => this.getRollPlanLeftoversTotal(endDate).map(this.convertOversToMap)
+                  .flatMap(toTotalOversMap => from(rolls)
+                    .map(roll => {
+                      const info: RollPlanInfo = {
+                        rollType: roll,
+                        currentRollLeftover: currentOversMap[roll.id],
+                        planBatches: batches[roll.id],
+                        weeklyLeftoverWithoutPlans: weeklyOversMap[roll.id],
+                        weeklyLeftoverTotal: weeklyTotalOversMap[roll.id],
+                        inTwoWeeksLeftoverWithoutPlans: toOversMap[roll.id],
+                        inTwoWeeksLeftoverTotal: toTotalOversMap[roll.id]
+                      }
+                      return info;
+                    })
+                    .toArray()
+                  )
+                )
+              )
+            )
+          )
+        )
+      );
+  }
+
+  private convertOversToMap(overs: RollLeftover[]): Map < number, RollLeftover > {
+    return new Map(overs.map(x => [x.rollTypeId, x] as[number, RollLeftover]));
+  }
 
   getBatchesByRange(fromDate: string, toDate: string): Observable < Map < number, RollPlanBatchResponse[] >> {
     const params = new HttpParams()
@@ -46,7 +94,7 @@ export class RollsPlanService {
     }).catch(httpErrorHandle);
   }
 
-  getRollPlanLeftoversTotal(toDate: string): Observable < RollLeftover > {
+  getRollPlanLeftoversTotal(toDate: string): Observable < RollLeftover[] > {
     const params = new HttpParams()
       .set('to_date', toDate);
     return this.http.get(this.urls.leftoverUrls, {
