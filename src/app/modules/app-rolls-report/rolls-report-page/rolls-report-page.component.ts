0f99f5d7b82aa023f41fdf1084b09d15cbae7a98
andrii <ryzhkov.andrii@gmail.com>
2018-12-24 01:08:48 +0200

    added empty rolls report page component

---
+++ src/app/modules/app-rolls-report/rolls-report-page/rolls-report-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rolls-report-page',
+  templateUrl: './rolls-report-page.component.html',
+  styleUrls: ['./rolls-report-page.component.css']
+})
+export class RollsReportPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
