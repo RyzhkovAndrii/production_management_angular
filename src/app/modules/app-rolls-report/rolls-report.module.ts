0e14504908e6617f58131d7036d97f43ae1825c6
andrii <ryzhkov.andrii@gmail.com>
2018-12-26 01:31:21 +0200

    added services and app-shared module to rolls report module

--- src/app/modules/app-rolls-report/rolls-report.module.ts
+++ src/app/modules/app-rolls-report/rolls-report.module.ts
@@ -4,15 +4,23 @@ import { CommonModule } from '@angular/common';
 import { RollsReportRoutingModule } from './rolls-report-routing.module';
 import { SecurityModule } from '../app-security/security.module';
 import { RollsReportPageComponent } from './rolls-report-page/rolls-report-page.component';
+import { AppSharedModule } from '../app-shared/app-shared.module';
+import { RollsReportModuleUrlService } from './services/rolls-report-module-url.service';
+import { RollsReportService } from './services/rolls-report.service';
 
 @NgModule({
     imports: [
         CommonModule,
         RollsReportRoutingModule,
-        SecurityModule
+        SecurityModule,
+        AppSharedModule
     ],
     declarations: [
         RollsReportPageComponent
+    ],
+    providers: [
+        RollsReportModuleUrlService,
+        RollsReportService
     ]
 })
 export class RollsReportModule { }
