db31240762ff9e27db2b55c991993c1583191211
andrii <ryzhkov.andrii@gmail.com>
2019-01-02 19:32:45 +0200

    added products report module base

---
+++ src/app/modules/app-products-report/products-report-page/products-report-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-report-page',
+  templateUrl: './products-report-page.component.html',
+  styleUrls: ['./products-report-page.component.css']
+})
+export class ProductsReportPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
